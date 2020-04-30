6baa26b1e1540a08e4ffb936f58c01969ef19952
James Hay <pokinoice@live.com>
2020-04-30 11:31:36 +0800

    Sweet

---
+++ src/6.ts
@@ -0,0 +1,7 @@
+
+
+export function renderButtonComponent (buttonId: string, container: HTMLElement, buttonContent: string) {
+
+  // Render a new button inside the container. If it already exists, only update its content.
+
+}
